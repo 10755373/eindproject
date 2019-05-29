8a21b5585d5a23cc37d6da34c66c1eba55ebda75
10755373 <rinusvangrunsven@gmail.com>
2019-05-29 17:31:43 +0200

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 fkcdsflkn
+# Rinus van Grunsven
+# 10755373
+
+import json
+import csv
+import pandas as pd
+
+input = "DP_LIVE_GDP.csv"
+output = "DP_LIVE_GDP.json"
+
+def read_csv(filename):
+    """
+    Read CSV and append a dict to a list
+    """
+    with open(input, "r") as csvfile:
+        # data_dict = {}
+        reader = pd.read_csv(csvfile, delimiter=';')
+        df = reader[["LOCATION", "TIME", "Value"]]
+        df_dict = df.to_dict(orient="split")
+        with open(output, "w") as jsonfile:
+            json.dump(df_dict, jsonfile)
+
+if __name__ == "__main__":
+    read_csv(input)
